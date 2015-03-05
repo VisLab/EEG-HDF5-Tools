0a85460a781e3b72cfed0d2f8598e33ab48aa2d0
Vincent <vandrada1248@gmail.com>
2015-03-05 12:37:34 -0600

    updated to latest version

--- c/channel_locations.h
+++ c/channel_locations.h
@@ -1,10 +1,11 @@
 #ifndef _CHANNEL_LOCATIONS_H
 #define _CHANNEL_LOCATIONS_H
 
+#include <stdio.h>
+
 /* structure for the channel locations compound data type */
 struct channel_locations {
     char  *labels;
-    char  *type;
     double theta;
     double radius;
     double X;
@@ -13,6 +14,7 @@ struct channel_locations {
     double sph_theta;
     double sph_phi;
     double sph_radius;
+    char  *type;
     double urchan;
     char  *ref;
 };
@@ -22,10 +24,10 @@ struct channel_locations {
  * \param chan the struct channel_locations to print
  */
 void print_channel_locations(struct channel_locations chan) {
-    printf("%-5s%-5s%12f%12f%12f%12f%12f%12f%12f%12f%12f%12s\n",
-           chan.labels, chan.type, chan.theta, chan.radius, chan.X, chan.Y,
-           chan.Z, chan.sph_theta, chan.sph_phi, chan.sph_radius, chan.urchan,
-           chan.ref);
+    printf("%5s%12f%12f%12f%12f%12f%12f%12f%12f%5s%12f%12s\n",
+           chan.labels, chan.theta, chan.radius, chan.X, chan.Y,
+           chan.Z, chan.sph_theta, chan.sph_phi, chan.sph_radius, chan.type,
+           chan.urchan, chan.ref);
 }
 
 #endif
